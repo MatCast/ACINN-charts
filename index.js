bd5b9c2b84993fa303aec299ea5acad7ef5f0bb8
MatCast <gozyssj4@gmail.com>
2020-03-07 13:47:07 +0100

    Added second chart and axes labels

--- index.js
+++ index.js
@@ -1,63 +1,14 @@
 //  http://meteo145.uibk.ac.at/innsbruck/7
 // ["rr", "dd", "tp", "p", "tl", "so", "ff", "datumsec"]
 const dataURL = 'data.json';
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
-// // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
-//
-// // We listen to the resize event
-// window.addEventListener('resize', () => {
-//     // We execute the same script as before
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
-//
-// window.onload = showViewport;
-// window.onresize = showViewport;
-//
-// function showViewport() {
-//   var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-//   var height= Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-// }
-
-function updateTemperature(chart) {
-  d3.json(dataURL).then(function(data) {
-    const temperature = document.querySelector('.temperature');
-    temperature.innerHTML = data.tl[data.tl.length - 1] + '&deg;C';
-    const epoch = data.datumsec[data.datumsec.length - 1];
-    removeNegativeRR(data.rr);
-    roundSunshine(data.so);
-    const date = moment(epoch).format('YYYY-MM-DD HH:mm');
-    const description = document.querySelector('.description');
-    description.innerHTML = 'Innsbruck University Temperature on <br>' + date;
-    const newDatSets = [
-      formatDataSet('tl', data),
-      formatDataSet('tp', data),
-      formatDataSet('rr', data),
-      formatDataSet('so', data)
-    ];
-    const newLabels = data.datumsec;
-    updateChart(chart, newLabels, newDatSets);
-  });
-}
-var ctx = document.getElementById('tempChart').getContext('2d');
-var chart = new Chart(ctx, {
+const tempChart = new Chart(document.getElementById('tempChart').getContext('2d'), {
   // The type of chart we want to create
   type: 'line',
 
   // The data for our dataset
   data: {
     labels: [],
-    datasets: [{
-      label: 'Temeprature',
-      backgroundColor: 'rgb(255, 255, 255, 0)',
-      borderColor: '#43A0C3',
-      data: [
-        // {x: new Date(), y: 175},
-        // {x: new Date(), y: 176},
-      ]
-    }]
+    datasets: []
   },
 
   // Configuration options go here
@@ -65,11 +16,6 @@ var chart = new Chart(ctx, {
     tooltips: {
       mode: 'x-axis',
       intersect: true,
-    },
-    elements: {
-      point: {
-        radius: 1
-      },
     },
     responsive: true,
     maintainAspectRatio: false,
@@ -95,6 +41,11 @@ var chart = new Chart(ctx, {
         },
       }],
       yAxes: [{
+        scaleLabel: {
+        display: true,
+        labelString: 'Temperature [\xB0C]',
+        fontColor: 'RGB(255, 69, 0)',
+      },
           id: 'Temperature',
           type: 'linear',
           position: 'left',
@@ -103,6 +54,11 @@ var chart = new Chart(ctx, {
           },
         },
         {
+          scaleLabel: {
+          display: true,
+          labelString: 'Precipitation [mm/h]',
+          fontColor: 'rgb(67, 160, 195)',
+        },
           id: 'Precipitation',
           type: 'linear',
           position: 'right',
@@ -114,6 +70,11 @@ var chart = new Chart(ctx, {
           },
         },
         {
+          scaleLabel: {
+          display: true,
+          labelString: 'Sunshine Duration [min/10min]',
+          fontColor: 'rgb(255, 215, 0)',
+        },
           id: 'Sunshine',
           type: 'linear',
           position: 'right',
@@ -129,13 +90,121 @@ var chart = new Chart(ctx, {
   }
 });
 
-updateTemperature(chart);
+const pressChart = new Chart(document.getElementById('pressChart').getContext('2d'), {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+    labels: [],
+    datasets: []
+  },
+
+  // Configuration options go here
+  options: {
+    tooltips: {
+      mode: 'x-axis',
+      intersect: true,
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 10,
+        top: 10,
+        bottom: 10
+      },
+    },
+    scales: {
+      xAxes: [{
+        scaleLabel: {
+        display: true,
+        labelString: 'Date'
+      },
+        type: 'time',
+        time: {
+          tooltipFormat:'YYYY-MM-DD HH:mm',
+          displayFormats: {
+            hour: 'MMM DD HH:mm'
+          }
+        },
+        gridLines: {
+          display: false
+        },
+      }],
+      yAxes: [{
+        scaleLabel: {
+        display: true,
+        labelString: 'Pressure [hPa]',
+        fontColor: 'RGB(71, 14, 143)',
+      },
+          id: 'Pressure',
+          type: 'linear',
+          position: 'left',
+          gridLines: {
+            display: false
+          },
+        },
+        {
+          scaleLabel: {
+          display: true,
+          labelString: 'Wind Direction [\xB0]',
+          fontColor: 'rgba(143, 71, 14, 0.8)',
+        },
+          id: 'Winddirection',
+          type: 'linear',
+          position: 'right',
+          ticks: {
+            min: 0
+          },
+          gridLines: {
+            display: false
+          },
+        },
+        {
+          scaleLabel: {
+          display: true,
+          labelString: 'Wind Speed [m/s]',
+          fontColor: 'rgb(14, 143, 71)',
+        },
+          id: 'Windspeed',
+          type: 'linear',
+          position: 'right',
+          ticks: {
+            min: 0
+          },
+          gridLines: {
+            display: false
+          },
+        }
+      ]
+    }
+  }
+});
+
+
+function updateTemperature(charts) {
+  d3.json(dataURL).then(function(data) {
+    const temperature = document.querySelector('.temperature');
+    temperature.innerHTML = data.tl[data.tl.length - 1] + '&deg;C';
+    const epoch = data.datumsec[data.datumsec.length - 1];
+    const date = moment(epoch).format('YYYY-MM-DD HH:mm');
+    const description = document.querySelector('.description');
+    description.innerHTML = 'Innsbruck University Temperature on <br>' + date;
+    updataTempChart(charts[0], data);
+    updataPressChart(charts[1], data);
+  });
+}
+
+
+updateTemperature([tempChart, pressChart]);
 let timer = new Date();
 
 function checkLastModified() {
   let now = new Date();
   if (now - timer > 5 * 60 * 1000) {
-    updateTemperature(chart);
+    updateTemperature([tempChart, pressChart]);
     timer = new Date();
   }
 }
@@ -196,45 +265,60 @@ const dataMap = {
     'color': 'rgb(67, 160, 195)',
     'fill': true,
     'yAxisID': 'Precipitation',
+    'showLine': true,
+    'pointRadius': 0,
   },
   'tl': {
     'label': 'Temperature',
     'color': 'RGB(255, 69, 0)',
     'fill': false,
     'yAxisID': 'Temperature',
+    'showLine': true,
+    'pointRadius': 0,
   },
   'tp': {
     'label': 'Dewpoint',
     'color': 'RGB(0, 128, 128)',
     'fill': false,
     'yAxisID': 'Temperature',
+    'showLine': true,
+    'pointRadius': 0,
   },
   'p': {
     'label': 'Pressure',
-    'color': 'RGB(75, 0, 130)',
+    'color': 'RGB(71, 14, 143)',
     'fill': false,
     'yAxisID': 'Pressure',
+    'showLine': true,
+    'pointRadius': 0,
   },
   'so': {
     'label': 'Sunshine',
     'color': 'rgb(255, 215, 0)',
     'fill': true,
     'yAxisID': 'Sunshine',
+    'showLine': true,
+    'pointRadius': 0,
   },
   'ff': {
     'label': 'Windspeed',
-    'color': 'RGB(46, 139, 87)',
+    'color': 'rgb(14, 143, 71)',
     'fill': false,
     'yAxisID': 'Windspeed',
+    'showLine': true,
+    'pointRadius': 0,
   },
   'dd': {
     'label': 'Winddirection',
-    'color': 'rgb(0, 0, 0)',
+    'color': 'rgba(143, 71, 14, 0.8)',
     'fill': false,
     'yAxisID': 'Winddirection',
+    'showLine': false,
+    'pointRadius': 2,
   }
 };
 
+// Creates a new dataset object to pass to addDataSet()
 function formatDataSet(key, jsonData) {
   const newDataset = {
     label: dataMap[key].label,
@@ -243,6 +327,31 @@ function formatDataSet(key, jsonData) {
     data: jsonData[key],
     fill: dataMap[key].fill,
     yAxisID: dataMap[key].yAxisID,
+    showLine: dataMap[key].showLine,
+    pointRadius: dataMap[key].pointRadius,
   };
   return newDataset;
+}
+
+function updataTempChart(chart, data) {
+  removeNegativeRR(data.rr);
+  roundSunshine(data.so);
+  const newDatSets = [
+    formatDataSet('tl', data),
+    formatDataSet('tp', data),
+    formatDataSet('rr', data),
+    formatDataSet('so', data)
+  ];
+  const newLabels = data.datumsec;
+  updateChart(chart, newLabels, newDatSets);
+}
+
+function updataPressChart(chart, data) {
+  const newDatSets = [
+    formatDataSet('p', data),
+    formatDataSet('ff', data),
+    formatDataSet('dd', data),
+  ];
+  const newLabels = data.datumsec;
+  updateChart(chart, newLabels, newDatSets);
 }
