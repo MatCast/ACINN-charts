2c0d0dfebf8d6f72001565dbebdd8cd445fc6b4b
MatCast <gozyssj4@gmail.com>
2020-03-07 22:37:41 +0100

    Added wind value

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
-//  http://meteo145.uibk.ac.at/innsbruck/7
-// ["rr", "dd", "tp", "p", "tl", "so", "ff", "datumsec"]
+//  data: http://meteo145.uibk.ac.at/innsbruck/7
+// attributes: ["rr", "dd", "tp", "p", "tl", "so", "ff", "datumsec"]
+// Map datasets as needed (colors, ecc)
+// from here you can configure any option for each of the weather vairable
+const dataMap = {
+  'rr': {
+    'label': 'Precipitation',
+    'color': 'rgb(67, 160, 195)',
+    'fill': true,
+    'yAxisID': 'Precipitation',
+    'showLine': true,
+    'pointRadius': 0,
+  },
+  'tl': {
+    'label': 'Temperature',
+    'color': 'RGB(255, 69, 0)',
+    'fill': false,
+    'yAxisID': 'Temperature',
+    'showLine': true,
+    'pointRadius': 0,
+  },
+  'tp': {
+    'label': 'Dewpoint',
+    'color': 'RGB(0, 128, 128)',
+    'fill': false,
+    'yAxisID': 'Temperature',
+    'showLine': true,
+    'pointRadius': 0,
+  },
+  'p': {
+    'label': 'Pressure',
+    'color': 'RGB(71, 14, 143)',
+    'fill': false,
+    'yAxisID': 'Pressure',
+    'showLine': true,
+    'pointRadius': 0,
+  },
+  'so': {
+    'label': 'Sunshine',
+    'color': 'rgb(255, 215, 0)',
+    'fill': true,
+    'yAxisID': 'Sunshine',
+    'showLine': true,
+    'pointRadius': 0,
+  },
+  'ff': {
+    'label': 'Windspeed',
+    'color': 'rgb(14, 143, 71)',
+    'fill': false,
+    'yAxisID': 'Windspeed',
+    'showLine': true,
+    'pointRadius': 0,
+  },
+  'dd': {
+    'label': 'Winddirection',
+    'color': 'rgba(48, 48, 48, 0.8)',
+    'fill': false,
+    'yAxisID': 'Winddirection',
+    'showLine': false,
+    'pointRadius': 2,
+  }
+};
+
 const dataURL = 'data.json';
 const tempChart = new Chart(document.getElementById('tempChart').getContext('2d'), {
   // The type of chart we want to create
@@ -44,7 +105,7 @@ const tempChart = new Chart(document.getElementById('tempChart').getContext('2d'
         scaleLabel: {
         display: true,
         labelString: 'Temperature [\xB0C]',
-        fontColor: 'RGB(255, 69, 0)',
+        fontColor: dataMap.tl.color,
       },
           id: 'Temperature',
           type: 'linear',
@@ -57,7 +118,7 @@ const tempChart = new Chart(document.getElementById('tempChart').getContext('2d'
           scaleLabel: {
           display: true,
           labelString: 'Precipitation [mm/h]',
-          fontColor: 'rgb(67, 160, 195)',
+          fontColor: dataMap.rr.color,
         },
           id: 'Precipitation',
           type: 'linear',
@@ -73,7 +134,7 @@ const tempChart = new Chart(document.getElementById('tempChart').getContext('2d'
           scaleLabel: {
           display: true,
           labelString: 'Sunshine Duration [min/10min]',
-          fontColor: 'rgb(255, 215, 0)',
+          fontColor: dataMap.so.color,
         },
           id: 'Sunshine',
           type: 'linear',
@@ -137,7 +198,7 @@ const pressChart = new Chart(document.getElementById('pressChart').getContext('2
         scaleLabel: {
         display: true,
         labelString: 'Pressure [hPa]',
-        fontColor: 'RGB(71, 14, 143)',
+        fontColor: dataMap.p.color,
       },
           id: 'Pressure',
           type: 'linear',
@@ -150,7 +211,7 @@ const pressChart = new Chart(document.getElementById('pressChart').getContext('2
           scaleLabel: {
           display: true,
           labelString: 'Wind Direction [\xB0]',
-          fontColor: 'rgba(143, 71, 14, 0.8)',
+          fontColor: dataMap.dd.color,
         },
           id: 'Winddirection',
           type: 'linear',
@@ -166,7 +227,7 @@ const pressChart = new Chart(document.getElementById('pressChart').getContext('2
           scaleLabel: {
           display: true,
           labelString: 'Wind Speed [m/s]',
-          fontColor: 'rgb(14, 143, 71)',
+          fontColor: dataMap.ff.color,
         },
           id: 'Windspeed',
           type: 'linear',
@@ -186,12 +247,15 @@ const pressChart = new Chart(document.getElementById('pressChart').getContext('2
 
 function updateTemperature(charts) {
   d3.json(dataURL).then(function(data) {
-    const temperature = document.querySelector('.temperature');
+    const temperature = document.querySelector('#temp-value');
     temperature.innerHTML = data.tl[data.tl.length - 1] + '&deg;C';
+    const wind = document.querySelector('#wind-value');
+    let ddWind = degToCard(data.dd[data.dd.length - 1]);
+    wind.innerHTML = data.ff[data.ff.length - 1] + ' ' + ddWind;
     const epoch = data.datumsec[data.datumsec.length - 1];
     const date = moment(epoch).format('YYYY-MM-DD HH:mm');
     const description = document.querySelector('.description');
-    description.innerHTML = 'Innsbruck University Temperature on <br>' + date;
+    description.innerHTML = 'Innsbruck University on <br>' + date;
     updataTempChart(charts[0], data);
     updataPressChart(charts[1], data);
   });
@@ -258,65 +322,7 @@ function roundSunshine(so) {
       so[i] = sun.toFixed(1);
   });
 }
-// Map datasets according as needed (colors, ecc)
-const dataMap = {
-  'rr': {
-    'label': 'Precipitation',
-    'color': 'rgb(67, 160, 195)',
-    'fill': true,
-    'yAxisID': 'Precipitation',
-    'showLine': true,
-    'pointRadius': 0,
-  },
-  'tl': {
-    'label': 'Temperature',
-    'color': 'RGB(255, 69, 0)',
-    'fill': false,
-    'yAxisID': 'Temperature',
-    'showLine': true,
-    'pointRadius': 0,
-  },
-  'tp': {
-    'label': 'Dewpoint',
-    'color': 'RGB(0, 128, 128)',
-    'fill': false,
-    'yAxisID': 'Temperature',
-    'showLine': true,
-    'pointRadius': 0,
-  },
-  'p': {
-    'label': 'Pressure',
-    'color': 'RGB(71, 14, 143)',
-    'fill': false,
-    'yAxisID': 'Pressure',
-    'showLine': true,
-    'pointRadius': 0,
-  },
-  'so': {
-    'label': 'Sunshine',
-    'color': 'rgb(255, 215, 0)',
-    'fill': true,
-    'yAxisID': 'Sunshine',
-    'showLine': true,
-    'pointRadius': 0,
-  },
-  'ff': {
-    'label': 'Windspeed',
-    'color': 'rgb(14, 143, 71)',
-    'fill': false,
-    'yAxisID': 'Windspeed',
-    'showLine': true,
-    'pointRadius': 0,
-  },
-  'dd': {
-    'label': 'Winddirection',
-    'color': 'rgba(143, 71, 14, 0.8)',
-    'fill': false,
-    'yAxisID': 'Winddirection',
-    'showLine': false,
-    'pointRadius': 2,
-  }
-};
+
 
 // Creates a new dataset object to pass to addDataSet()
 function formatDataSet(key, jsonData) {
@@ -354,4 +360,41 @@ function updataPressChart(chart, data) {
   ];
   const newLabels = data.datumsec;
   updateChart(chart, newLabels, newDatSets);
+}
+
+// Transforms wind directions from degrees to cardinal
+function degToCard(deg){
+  if (deg>11.25 && deg<33.75){
+    return "NNE";
+  }else if (deg>33.75 && deg<56.25){
+    return "ENE";
+  }else if (deg>56.25 && deg<78.75){
+    return "E";
+  }else if (deg>78.75 && deg<101.25){
+    return "ESE";
+  }else if (deg>101.25 && deg<123.75){
+    return "ESE";
+  }else if (deg>123.75 && deg<146.25){
+    return "SE";
+  }else if (deg>146.25 && deg<168.75){
+    return "SSE";
+  }else if (deg>168.75 && deg<191.25){
+    return "S";
+  }else if (deg>191.25 && deg<213.75){
+    return "SSW";
+  }else if (deg>213.75 && deg<236.25){
+    return "SW";
+  }else if (deg>236.25 && deg<258.75){
+    return "WSW";
+  }else if (deg>258.75 && deg<281.25){
+    return "W";
+  }else if (deg>281.25 && deg<303.75){
+    return "WNW";
+  }else if (deg>303.75 && deg<326.25){
+    return "NW";
+  }else if (deg>326.25 && deg<348.75){
+    return "NNW";
+  }else{
+    return "N";
+  }
 }
